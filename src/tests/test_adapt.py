bfca9b818a64ac638d0882929ac01bcaed099db2
Mike Thornton <msthornton@gmail.com>
2012-04-14 05:01:35 -0500

    Adapt incomming parameters

--- src/tests/test_adapt.py
+++ src/tests/test_adapt.py
@@ -1,4 +1,4 @@
-from anticipate import adapt, adapter
+from anticipate import adapt, adapter, anticipate
 
 def test_mro():
     class Foo(object):
@@ -17,6 +17,7 @@ def test_mro():
     s = adapt.adapt(bar, str)
 
     assert s == 'Adapted to string'
+    assert s == 'Adapted to string'
 
     class Zip(object):
         def __init__(self, data):
@@ -33,6 +34,32 @@ def test_mro():
 
     assert s == 'Adapted to zip'
 
-    assert False
+def test_adapt_params():
+    @anticipate(foo=str, bar=int)
+    def test(foo, bar, zing):
+        return foo, bar, zing
+
+    class FizzBuzz(object):
+        def __str__(self):
+            return 'fizzbuzz'
+
+        def __int__(self):
+            return 22
+
+    @adapter((basestring, int, float, FizzBuzz), (basestring, int, float))
+    def to_int(obj, to_cls):
+        return to_cls(obj)
+
+    assert test(1, 2.3, 'fizz') == ('1', 2, 'fizz')
+    assert test(1, 2.3, zing='fizz') == ('1', 2, 'fizz')
+    assert test(1, bar=2.3, zing='fizz') == ('1', 2, 'fizz')
+    assert test(foo=1, bar=2.3, zing='fizz') == ('1', 2, 'fizz')
+
+    assert test('hi', '1', str) == ('hi', 1, str)
+
+    a = FizzBuzz()
+    assert test(a, a, a) == ('fizzbuzz', 22, a)
+
+
 
 
