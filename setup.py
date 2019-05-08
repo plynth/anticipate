58083650fcf918847f00afd78767d5049c7a69b2
Michael Thornton <mthornton@pulsesecure.net>
2019-05-08 10:08:36 -0500

    Fixed some packaging issues
    - Bumped version to 0.9.1 in order to upload the fix to pypi
    - Pre-process _README.rst to import includes and generate a README.rst. This is because github and pypi do not process includes.
    - Added trove classifiers for Python 3
    - Added build-requirements.txt and a `build` and `publish` command for setup.py
    - Removed deprectated fabfile.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
-import os
+from subprocess import check_call
 import sys
 from setuptools import setup
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist bdist_wheel')
-    os.system('twine upload dist/*')
+if sys.argv[-1] in ('build', 'publish'):
+    check_call(
+        # Use `which` because rst_inc does not (in 1.04) have a sh-bang
+        # to execute Python automatically.
+        'python `which rst_inc.py` include -s ./_README.rst -t ./README.rst', shell=True)
+    check_call('python setup.py sdist bdist_wheel', shell=True)
+    if sys.argv[-1] == 'publish':
+        check_call('twine upload dist/*', shell=True)
     sys.exit()
+
 
 tests_require = [
     'pytest',
@@ -32,10 +38,14 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Operating System :: OS Independent',
         'License :: OSI Approved :: BSD License',
     ],
     long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
     tests_require=tests_require,
     extras_require=dict(test=tests_require),
     setup_requires=['pytest-runner'] if {'pytest', 'test'}.intersection(sys.argv) else [],
